c54d0c78e2aa6369d8d3bf140372eb3fc0b28ea2
annagaom <122282732+annagaom@users.noreply.github.com>
2024-04-01 15:53:40 +0300

    etusivu ja kayttaja sivu päivistys

--- javascript/7Kayttaja.js
+++ javascript/7Kayttaja.js
@@ -1,1 +1,26 @@
-'use script';
+
+
+
+// // kun painaa registeroidy button, niin tulee registeri formi ikkuna
+// registerForm.style.display = 'none';
+// document.querySelector('#registerBtn').addEventListener('click', () => {
+//     registerForm.style.display = 'block';
+// });
+// function registerUser(e) {
+//     e.preventDefault();
+
+//     const firstname = registerForm['etunimi'].value;
+//     const laskname = registerForm['sukunimi'].value;
+
+//     const email = registerForm['email'].value;
+//     const phone = registerForm['puhelin'].value;
+//     const password = registerForm['password'].value;
+
+//     auth.createUserWithEmailAndPassword(email, password).then(cred => {
+//         console.log(cred.user);
+//         registerForm.reset();
+//     });
+// }
+// registerForm.addEventListener('submit', registerUser);
+
+
