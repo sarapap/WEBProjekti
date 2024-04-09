13e4362709629f282a29986ef388f5d4aef07005
annagaom <122282732+annagaom@users.noreply.github.com>
2024-04-09 20:45:30 +0300

    muutettu html omaan kansiioon, a korjattu js linkki

--- javascript/1Etusivu.js
+++ javascript/1Etusivu.js
@@ -1,13 +1,12 @@
 'use script';
-
 document.getElementById("kieli").addEventListener("change", function() {
   var selectedLanguage = this.value;
   if (selectedLanguage === 'FI') {
-      window.location.href = '../1Etusivu.html';
+      window.location.href = './fi/1Etusivu.html';
   } else if (selectedLanguage === 'EN') {
-      window.location.href = '../en/1Etusivu.html';
+      window.location.href = './en/1Etusivu_en.html';
   } else if (selectedLanguage === 'CN') {
-      window.location.href = "/cn/1Etusivu.html";
+      window.location.href = "./1Etusivu_cn.html";
   }
 });
 
