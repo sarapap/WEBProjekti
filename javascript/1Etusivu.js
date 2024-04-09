f40dfd37e3f7c09c824c8b0cbd60db5b557d6499
annagaom <122282732+annagaom@users.noreply.github.com>
2024-04-09 19:29:11 +0300

    lisää kieli valintoa

--- javascript/1Etusivu.js
+++ javascript/1Etusivu.js
@@ -1,5 +1,17 @@
 'use script';
 
+document.getElementById("kieli").addEventListener("change", function() {
+  var selectedLanguage = this.value;
+  if (selectedLanguage === 'FI') {
+      window.location.href = '../1Etusivu.html';
+  } else if (selectedLanguage === 'EN') {
+      window.location.href = '../en/1Etusivu.html';
+  } else if (selectedLanguage === 'CN') {
+      window.location.href = "/cn/1Etusivu.html";
+  }
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.getElementById("menuButton");
     const menuDropdown = document.getElementById("menuDropdown");
