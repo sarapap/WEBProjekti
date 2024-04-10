c1b51de551959569a5d7b89bd9201c142df8a2e5
sarapappila <sara.pappila@metropolia.fi>
2024-04-10 20:55:30 +0300

    Lisätään kielet ja html

--- javascript/1Etusivu.js
+++ javascript/1Etusivu.js
@@ -3,11 +3,17 @@
 document.getElementById("kieli").addEventListener("change", function () {
     var selectedLanguage = this.value;
     if (selectedLanguage === 'FI') {
-        window.location.href = '../1Etusivu.html';
+        window.location.href = '../fi/1Etusivu.html';
     } else if (selectedLanguage === 'EN') {
-        window.location.href = '../en/1Etusivu.html';
+        window.location.href = '../en/1Etusivu_en.html';
     } else if (selectedLanguage === 'CN') {
-        window.location.href = "/cn/1Etusivu.html";
+        window.location.href = "/cn/1Etusivu_cn.html";
+    }
+    else if (selectedLanguage === 'ET') {
+        window.location.href = "/et/1Etusivu_et.html";
+    }
+    else if (selectedLanguage === 'SV') {
+        window.location.href = "/sv/1Etusivu_sv.html";
     }
 });
 
--- javascript/5Yhteydenotto.js
+++ javascript/5Yhteydenotto.js
@@ -1,5 +1,22 @@
 'use strict';
 
+document.getElementById("kieli").addEventListener("change", function () {
+    var selectedLanguage = this.value;
+    if (selectedLanguage === 'FI') {
+        window.location.href = '../fi/1Etusivu.html';
+    } else if (selectedLanguage === 'EN') {
+        window.location.href = '../en/1Etusivu_en.html';
+    } else if (selectedLanguage === 'CN') {
+        window.location.href = "/cn/1Etusivu_cn.html";
+    }
+    else if (selectedLanguage === 'ET') {
+        window.location.href = "/et/1Etusivu_et.html";
+    }
+    else if (selectedLanguage === 'SV') {
+        window.location.href = "/sv/1Etusivu_sv.html";
+    }
+});
+
 const map = L.map('map').setView([60.2826627, 25.0101836], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
