6de6ce4d2ab1862644beec8812b87dcef7717082
annagaom <122282732+annagaom@users.noreply.github.com>
2024-04-14 20:13:10 +0300

    poistettu ylimäärästä kielen valinnasta js

--- javascript/2AboutUs.js
+++ javascript/2AboutUs.js
@@ -1,18 +1,3 @@
 'use strict';
 
-document.getElementById("kieli").addEventListener("change", function () {
-    var selectedLanguage = this.value;
-    if (selectedLanguage === 'FI') {
-        window.location.href = '../fi/1Etusivu.html';
-    } else if (selectedLanguage === 'EN') {
-        window.location.href = '../en/1Etusivu_en.html';
-    } else if (selectedLanguage === 'CN') {
-        window.location.href = "../cn/1Etusivu_cn.html";
-    }
-    else if (selectedLanguage === 'ET') {
-        window.location.href = "../et/1Etusivu_et.html";
-    }
-    else if (selectedLanguage === 'SV') {
-        window.location.href = "../sv/1Etusivu_sv.html";
-    }
-});
+
