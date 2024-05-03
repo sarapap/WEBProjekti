f277736b1396b13d4b6f2a2a8adf137fafb397e6
annagaom <122282732+annagaom@users.noreply.github.com>
2024-05-03 09:25:11 +0300

    sanan korjaus

--- javascript/3MenuPage1.js
+++ javascript/3MenuPage1.js
@@ -2,7 +2,7 @@
 
 const handleNewValue = async () => {
   const alatyyppi = getSelectedAlaTyyppi();
-  await updateSubtypes(alaTyyppi);
+  await updateSubtypes(alatyyppi);
 };
 
 const updateSelectedAlatyyppi = () => {
